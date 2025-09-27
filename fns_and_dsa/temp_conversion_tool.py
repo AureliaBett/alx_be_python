647c6bc1b0dcd21685e12f35c77b17004d3a3404
AureliaBett <cbettaurelia@gmail.com>
2025-09-27 04:27:52 +0300

    Checks for Definition of global conversion factors

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,5 +1,5 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR =(5/9)
-CELSIUS_TO_FAHRENHEIT_FACTOR =(9/5)
+FAHRENHEIT_TO_CELSIUS_FACTOR =5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR =9/5
 while True:
     def convert_to_celsius(fahrenheit):    
         temperature_in_celsious = temperature * FAHRENHEIT_TO_CELSIUS_FACTOR        
