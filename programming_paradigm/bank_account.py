5c7ff0eb38fdce0db40579354226dab63ed66705
AureliaBett <cbettaurelia@gmail.com>
2025-10-08 21:51:10 +0300

    Still fixing bug on current balance

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -19,7 +19,7 @@ class BankAccount:
             return False
         
     def display_balance(self):
-       Current_Balance = f"Your Current Balance: {self.account_balance}"
+       Current_Balance = f"Current Balance: {self.account_balance}"
        print(Current_Balance)
    
         
